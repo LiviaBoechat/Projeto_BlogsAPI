3c30d292ba5e47d37c22ddd87571c92ffe7b89e0
LiviaBoechat <limaboechatti@gmail.com>
2023-06-12 19:23:40 -0300

    req. 03 ok

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,16 +1,27 @@
 
 const { use } = require('frisby');
+const jwt = require('jsonwebtoken');
 const loginService = require('../services/loginService');
+
+const secret = process.env.JWT_SECRET;
 
 const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
     const { type, message } = await loginService.signIn(email, password);
    
-    if (type)
-      return res.status(type).json({ message });
+    if (type) return res.status(type).json({ message });
 
-    return res.status(200).json(message);
+    const jwtConfig = {
+        expiresIn: '7d',
+        algorithm: 'HS256',
+      };
+    
+    const token = jwt.sign({ data: { userId: message.id } }, secret, jwtConfig);  
+    
+    
+    return res.status(200).json({ token });
+
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: 'Algo deu errado' });
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,8 @@
 const { User } = require('../models');
-const token = require('../utils/token');
 
 const signIn = async (email, password) => {
   const user = await User.findOne({ where: { email } });
-  console.log(user);
+
   if(!user || user === null || user.password !== password) {
     return { type: 400,  message: 'Invalid fields' }
   } 
