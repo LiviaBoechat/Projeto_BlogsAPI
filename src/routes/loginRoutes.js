fb5b9307ed87cf1ed623966f73076678862e95c8
LiviaBoechat <limaboechatti@gmail.com>
2023-06-13 18:48:04 -0300

    lint2

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 const loginValidation = require('../middlewares/loginValidation');
 
-
 router.post('/', loginValidation, loginController.signIn);
 
 module.exports = router;
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -3,12 +3,12 @@ const { User } = require('../models');
 const signIn = async (email, password) => {
   const user = await User.findOne({ where: { email } });
 
-  if(!user || user === null || user.password !== password) {
-    return { type: 400,  message: 'Invalid fields' }
+  if (!user || user === null || user.password !== password) {
+    return { type: 400, message: 'Invalid fields' };
   } 
 
-  return { type: null,  message: user };
-}
+  return { type: null, message: user };
+};
 
 module.exports = {
     signIn,
